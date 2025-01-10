a3f74525c664ca9ac004d83bba6761e83d58ddf9
MLRV13 <matias.l@v13consultoria.com>
2025-01-10 16:42:40 +0100

    xd

--- TestGithubActions/obj/Debug/net8.0/TestGithubActions.AssemblyInfo.cs
+++ TestGithubActions/obj/Debug/net8.0/TestGithubActions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGithubActions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3694169e2ce722b22fa73f2d4d70fbee11e4a98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76a2164496546bbc5831fe72987c5fb7bd1f36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGithubActions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGithubActions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
